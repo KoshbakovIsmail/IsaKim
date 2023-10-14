343f1eb99e8582ad51fb1c4b0de4ed239ccfebd6
KosbakovIsmail <kosbakovismail@gmail.com>
2023-10-14 22:09:00 +0300

    Message 2

--- src/Main.java
+++ src/Main.java
@@ -4,10 +4,10 @@ public class Main {
         var rus1 = "Кошбаков ";
         var rus2 = "Исмаил ";
         var rus3 = "Муктарович ";
-        System.out.println("Hello World!- " + rus);
-        System.out.println("Koshbakov- " + rus1);
-        System.out.println("Ismail- " + rus2);
-        System.out.println("Muktarovich- " + rus3);
+        System.out.println(rus);
+        System.out.println(rus1);
+        System.out.println(rus2);
+        System.out.println(rus3);
 
     }
 }
